2298adb6558a68d1ab65734037a8c99fb6c60351
mahcloud <mike.hoffert@gmail.com>
2015-07-07 21:07:37 -0600

    Users list page

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,47 @@
+class Admin::UsersController < AdminController
+  load_and_authorize_resource
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    params[:user].delete(:password) if params[:user][:password].blank?
+    params[:user].delete(:password_confirmation) if params[:user][:password_confirmation].blank?
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
