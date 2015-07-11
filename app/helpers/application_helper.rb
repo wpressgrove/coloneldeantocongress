455a4c3cab4450ff3e66832ad06ac13f62a32ed4
mahcloud <mike.hoffert@gmail.com>
2015-07-11 10:13:45 -0600

    Load real event data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     flag = true if controllers.include? @current_controller
     flag
   end
+
+  def next_event
+    Event.where("time >= ?", Time.now.beginning_of_day).order("time ASC").first
+  end
 end
