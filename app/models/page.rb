d61c8dedbe897c12a6433cac4da0d27f35b43fd9
Mike Hoffert <mike.hoffert@gmail.com>
2015-08-02 23:22:13 -0600

    Fix MySQL vs PostgreSQL syntax error

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,7 +6,7 @@ class Page < ActiveRecord::Base
 
   validates :name, uniqueness: true, presence: true
 
-  scope :available_pages, ->(include_id) { joins("LEFT JOIN nav_tabs ON(nav_tabs.page_id = pages.id)").where("nav_tabs.page_id IS NULL OR pages.id = '#{include_id}'").where.not(slug: 'home')}
+  scope :available_pages, ->(include_id) { joins("LEFT JOIN nav_tabs ON(nav_tabs.page_id = pages.id)").where("nav_tabs.page_id IS NULL OR pages.id = ?", include_id).where.not(slug: 'home')}
   before_save :set_slug
 
   def set_slug
