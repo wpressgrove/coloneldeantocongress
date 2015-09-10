c2a1a3dbc349e46c6f0d24ab4b674a3a3a7f76be
Mike Hoffert <mike.hoffert@gmail.com>
2015-09-09 22:38:23 -0600

    Email colonel Dean too

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -2,5 +2,6 @@ class QuestionMailer < ActionMailer::Base
   def question_email(question)
     @question = question
     mail(to: ENV['ADMIN_EMAIL'], from: @question.email, subject: 'Someone has asked Colonel Andre Dean a Question')
+    mail(to: ENV['EMAIL'], from: @question.email, subject: 'Someone has asked Colonel Andre Dean a Question')
   end
 end
