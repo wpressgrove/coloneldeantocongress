3dea7cebc652faa7a42b906dee461441016380aa
Mike Hoffert <mike.hoffert@gmail.com>
2015-09-09 22:25:51 -0600

    Forget self

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -1,5 +1,5 @@
 class QuestionMailer < ActionMailer::Base
-  def question_email(question)
+  def self.question_email(question)
     @question = question
     mail(to: ENV['ADMIN_EMAIL'], from: question.email, subject: 'Someone has asked Colonel Andre Dean a Question')
   end
