75575aae5e3f7459f2e17728657deedd213b02b4
Mike Hoffert <mike.hoffert@gmail.com>
2015-09-12 18:35:37 -0600

    Default from

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -1,13 +1,15 @@
 class QuestionMailer < ActionMailer::Base
+  default from: 'no-reply@coloneldeantocongress.org'
+
   QUESTION_SUBJECT = 'Someone has asked Colonel Andre Dean a Question'
 
   def admin_email(question)
     @question = question
-    mail(to: ENV['ADMIN_EMAIL'], from: @question.email, subject: QUESTION_SUBJECT)
+    mail(to: ENV['ADMIN_EMAIL'], subject: QUESTION_SUBJECT)
   end
 
   def question_email(question)
     @question = question
-    mail(to: ENV['EMAIL'], from: @question.email, subject: QUESTION_SUBJECT)
+    mail(to: ENV['EMAIL'], subject: QUESTION_SUBJECT)
   end
 end
